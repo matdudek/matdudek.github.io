a998da48ad62337547d1e4693161fa29f53c1eed
matdudek <mateuszdudek960521@gmail.com>
2018-01-24 00:48:51 +0100

    version 0.1.3

--- Ball_In_Hole/javascripts/script.js
+++ Ball_In_Hole/javascripts/script.js
@@ -2,54 +2,42 @@ window.addEventListener("deviceorientation", manipulation,false);
 let width = window.innerWidth;
 let height = window.innerHeight;
 let canvas = document.getElementById("myCanvas")
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = 600 
+canvas.height = 600
 let ctx = canvas.getContext("2d");
 let ball = new Ball(200,200,ctx)
-let BluePoint;
-let GreenPoint;
-let RedPoint;
+let hole ;
 let beta = 0;
 let gamma = 0;
-let timeLeft = 30;
-let points = 0;
+
+
 function appStart(){
-    BluePoint = new BlueHole();
-    GreenPoint = new GreenHole();
-    RedPoint = new RedHole();
-    RedPoint.checkPosition();
+    hole = new Hole();
     move();
     time();
-    document.getElementById("points").innerHTML = "Points: 0";
-    document.getElementById("info").innerHTML = "Time Left: 30";
-    document.getElementById("startGame").style.display = "none";
 }
-
+appStart();
 
 function manipulation(e){
-    if(timeLeft>0){
-        beta = e.beta;
-        gamma= e.gamma;
-        if(e.beta> 90) {
-            beta = 90;
-        }else if(e.beta< -90){
-            beta = -90;
-        }
-        if(e.gamma> 90) {
-            gamma = 90;
-        }else if(e.gammaa< -90){
-            gamma = -90;
-        }
-        ball.physics(beta,gamma);
+    beta = e.beta;
+    gamma= e.gamma;
+    if(e.beta> 90) {
+        beta = 90;
+    }else if(e.beta< -90){
+        beta = -90;
     }
+    if(e.gamma> 90) {
+        gamma = 90;
+    }else if(e.gammaa< -90){
+        gamma = -90;
+    }
+    ball.physics(beta,gamma);
 }
 
 
 function move(){
     ctx.clearRect(0, 0, canvas.width, canvas.width); 
-    BluePoint.draw();
-    GreenPoint.draw();
-    RedPoint.draw();
+    hole.draw();
     ball.draw();
     ball.physics();
     ball.colisions();
@@ -59,16 +47,14 @@ function move(){
 
 
 function time(){
-    
+    let time = 3000
     let timer = setInterval(function(){
-        if(timeLeft>0){
-            timeLeft-=1; 
-        document.getElementById("info").innerHTML = "Time Left:" + timeLeft
+        if(time>0){
+        time-=1; 
+        document.getElementById("info").innerHTML = "Time Left:" + time
         }
         else{
-            clearInterval(timer);
-            alert("You lost, Point: "+ points);
-            document.getElementById("startGame").style.display = "block";
+            timer = null;
         }
-    }, 1000);
+    }, 10);
 }
